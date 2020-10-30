68f979bfbf3726c77f241daad27271e750d57329
Walter Fritsche <wfritsche95@protonmail.com>
2020-10-30 15:46:57 -0500

    Working terminal game!

--- main.rb
+++ main.rb
@@ -7,6 +7,7 @@ class Board
   end
 
   def display_board
+    puts "\n"
     puts "#{board['topl']} | #{board['topm']} | #{board['topr']}"
     puts "-" * ("#{board['midl']} | #{board['midm']} | #{board['midr']}".length + 1)
     puts "#{board['midl']} | #{board['midm']} | #{board['midr']}"
@@ -69,5 +70,32 @@ end
 board = Board.new
 
 puts "Let's start with getting player one's info"
+puts "\nWhat is you name? "
+name1 = gets.chomp
+puts "\nWhat do you want your marker to be? "
+marker1 = gets.chomp[0]
+player1 = Player.new(name1, marker1)
+
+puts "Let's start with getting player two's info"
+puts "\nWhat is you name? "
+name2 = gets.chomp
+puts "\nWhat do you want your marker to be? "
+marker2 = gets.chomp[0]
+player2 = Player.new(name2, marker2)
+
+players = [player1, player2]
+counter = 0
+until board.fullboard? do
+  turn = counter % 2
+  puts "#{players[turn].name} make your play: "
+  choice = gets.chomp
+  if board.board.keys.include?(choice) && board.board[choice] == ' '
+    board.board_play(players[turn], choice)
+    counter += 1
+  else
+    redo
+  end
+end
+abort("Board Full: Tie")
 
 
